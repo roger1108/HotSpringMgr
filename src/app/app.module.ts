cf98d31b5274e2b8d15311a0a3920187618fbed0
roger1108 <panel.pan@hotmail.com>
2017-12-12 20:17:56 +0800

    chapter 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
+import { StarsComponent } from './stars/stars.component';;
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+import {Routes, RouterModule} from "@angular/router";
+import { ProductService } from './shared/product.service';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'product/:productId', component: ProductDetailComponent}
+  
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    ProductComponent,
+    StarsComponent,
+    CarouselComponent,
+    FooterComponent,
+    ProductDetailComponent,
+    HomeComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  exports: [RouterModule],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  title='测试Angular';
+
+  // test(){
+  //     $("xxx").show( );
+  // }
+}
